3995c2f5e355174d4fc514882ea48f3e40e31cf7
donielmaker <daniel.schmidt0204@gmail.com>
2025-01-29 23:34:39 +0100

    2nd working version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::widget::{text, Text, Column, column, text_input, button, Button, row, Row};
+use iced::widget::{text,  column, text_input, row, TextInput, button, keyed, keyed_column};
 
 pub fn main() -> iced::Result {
     // iced::run("A cool counter", Counter::update, Counter::view)
@@ -6,103 +6,61 @@ pub fn main() -> iced::Result {
     iced::application("Single Task", State::update, State::view).theme(|_| iced::Theme::CatppuccinMacchiato).run()
 }
 
-// #[derive(Default)]
-// struct Counter {
-//     value: i64,
+// #[derive(Clone, Default, Debug)]
+// struct Task {
+//     content: String,
+//     id: uuid::Uuid,
 // }
 //
-// #[derive(Debug, Clone, Copy)]
-// enum Message {
-//     Increment,
-//     Decrement,
-// }
+// impl Task {
+//     fn view(&self) -> iced::Element<Message> {
+//         let content = text_input("", &self.content).width(iced::Length::Fixed(400.0)).on_input(Message::Edit());
 //
-// impl Counter {
-//     fn update(&mut self, message: Message) {
-//         match message {
-//             Message::Increment => {
-//                 self.value += 1;
-//             }
-//             Message::Decrement => {
-//                 self.value -= 1;
-//             }
-//         }
-//     }
+//         let del = button("del").on_press(Message::Remove(self.id));
 //
-//     fn view(&self) -> Column<Message> {
-//         column![
-//             button("Increment").on_press(Message::Increment),
-//             text(self.value).size(50),
-//             button("Decrement").on_press(Message::Decrement)
-//         ]
-//         .padding(20)
-//         .align_x(Center)
+//         row![content, del].into()
 //     }
 // }
-
-#[derive(Clone, Default, Debug)]
-struct Task {
-    content: String,
-    id: uuid::Uuid,
-}
-
-impl Task {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Edit(new_content) => {
-                self.content = new_content; 
-            }
-            _ => {}
-        }
-    }
-
-    fn view(&self) -> iced::Element<Message> {
-        let content = text_input("", &self.content).width(iced::Length::Fixed(400.0)).on_input(Message::Edit);
-
-        let del = button("del").on_press(Message::Remove(self.id));
-
-        row![content, del].into()
-    }
-}
 
 #[derive(Debug, Clone)]
 enum Message {
     Add(String),
-    Remove(uuid::Uuid),
+    Remove(usize),
     Input(String),
-    Edit(String),
 }
 
 #[derive(Default, Debug)]
 struct State {
     search: String,
-    todos: Vec<Task>,
+    todos: Vec<String>,
 }
 
 impl State {
     fn update(&mut self, message: Message) {
         match message {
             Message::Add(content) => {
-                self.todos.push(Task {content, id: uuid::Uuid::new_v4()}) 
+                self.todos.push(content);
             }
-            Message::Remove(id) => {
-                if let Some(index) = self.todos.iter().position(|x| x.id == id) {
-                    self.todos.remove(index);
-                }
+            Message::Remove(index) => {
+                self.todos.remove(index);
             }
             Message::Input(search) => {
                 self.search = search
             }
-            _ => {}
         }
     }
 
     fn view(&self) -> iced::Element<Message> {
-        let search_bar: iced::widget::TextInput<Message> = text_input("Search...", &self.search).on_input(Message::Input).on_submit(Message::Add(self.search.clone()));
+        let search_bar: TextInput<Message> = text_input("Search...", &self.search).on_input(Message::Input).on_submit(Message::Add(self.search.clone()));
 
-        let tasks: Vec<(usize, &Task)> = self.todos.iter().enumerate().map(|(i, task)| (i, task)).collect();
-
-        let tasks: iced::widget::keyed::Column<usize, Message> = iced::widget::keyed_column(tasks.iter().map(|(key, task)| (*key, task.view())));
+        let tasks: keyed::Column<usize, Message> = keyed_column(
+            self.todos
+                .iter()
+                .enumerate()
+                .map(|(key, task)| {
+                    (key, row![text(task).width(iced::Length::Fill), button("Del").on_press(Message::Remove(key))].into())
+                })
+        );
 
         column![search_bar, tasks].into()
     }
